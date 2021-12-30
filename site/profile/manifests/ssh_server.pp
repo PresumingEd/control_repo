2f05fa823c89d5f1e0451931a76e6a0dfb905598
PresumingEd <presuming_edd@hotmail.com>
2021-12-30 23:46:48 +0000

    Update ssh_server.pp
    corrected ssh key path to actual fingerprint

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '/etc/puppetlabs/code/environments/production/keys/ssh_server.pub',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCgeICntc3x0PZO2DoDCTiaZRSvlVDn91ffLEHTcKNzi4q7V1Y2W4kwyIo3KkYDA2u/GV4fcmH7/K3wdotquaWpx2czl3gdYuFUT183IxuRIC7csGbeckjqKGqamv51p7dIBMqppyF73pL3ndHa/Ht+SsMfwuQ6598oJH7ahWgyIK+3AdYcxaPps0VTSIymCqNQ1otpU+JHj3HclGOQyt6gvneNVaelTyHX2Ulek2bWm9A9hKN0FA8EFoDPKz3MQE/YUtdfuvbGY4calDb2OhG3Vs1Sg4Wr2rFjkdbwlFiBhkoPTsiKXLcScfcy3W/h+dYh6b7rp+xlNOIy+nL1xcqH',
 	}  
 }
